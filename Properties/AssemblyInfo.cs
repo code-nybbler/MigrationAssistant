91c573d2545b05463ba61963d3c9e109f3abef1d
Luke Thomas <116568876+code-nybbler@users.noreply.github.com>
2023-07-18 11:21:34 -0400

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MigrationAssistant")]
-[assembly: AssemblyDescription("Aids in the mapping process of migration")]
+[assembly: AssemblyDescription("Assists the user in the process of mapping legacy SQL database schemas to Microsoft Dataverse tables and columns, as well as exporting mapping documents and SQL queries to be used in migration scripts/packages.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Luke Thomas")]
 [assembly: AssemblyProduct("Migration Assistant")]
